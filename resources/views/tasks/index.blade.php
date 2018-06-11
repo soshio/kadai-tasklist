226678383112cca75412f085a598606d2011dd76
S.S <tectest58@gmail.com>
2018-06-11 07:36:05 +0000

    add index.blade.php

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{ $task->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+@endsection
