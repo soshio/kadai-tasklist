4fb5552c2de46f107bd080ae03f0af20f7cf6a1f
S.S <tectest58@gmail.com>
2018-06-11 08:59:37 +0000

    add show_link

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,7 +7,7 @@
     @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
-                <li>{{ $task->content }}</li>
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
             @endforeach
         </ul>
     @endif
