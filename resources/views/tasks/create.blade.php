c70ab3d019eb698529dac6c91b5c0f12286d421f
S.S <tectest58@gmail.com>
2018-06-12 07:04:19 +0000

    validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,12 +4,12 @@
 
     <h1>タスク新規作成ページ</h1>
 
-     {!! Form::model($task,['route'=>'tasks.store']) !!}
+    {!! Form::model($task,['route'=>'tasks.store']) !!}
 
-{!! Form::label('content','タスク:')!!}
-{!! Form::text('content') !!}
+    {!! Form::label('content','タスク:')!!}
+    {!! Form::text('content') !!}
 
-{!! Form::submit('投稿') !!}
+    {!! Form::submit('投稿') !!}
 
 
     {!! Form::close() !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
 
-
  {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
         {!! Form::label('content', 'メッセージ:') !!}
