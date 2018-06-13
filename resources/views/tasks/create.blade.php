7288edcc1a9d414f93fb183ce44a379792cafdd0
S.S <tectest58@gmail.com>
2018-06-13 13:10:53 +0000

    add grid

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,34 +4,34 @@
 
   <h1>タスク新規作成ページ</h1>
 
-<div class="row">
-	<div class="col-xs-6">
-  {!! Form::model($task,['route'=>'tasks.store']) !!}
-
-	<div class="form-group">
-    {!! Form::label('title','タイトル')!!}
-    {!! Form::text('title',null,['class'=>'form-control	']) !!}
-     </div>
-     <div class="form-group">
-    {!! Form::label('content','タスク:')!!}
-    {!! Form::text('content',null,['class'=>'form-control']) !!}
-    </div>
-	 <div class="form-group">
-      {!! Form::label('statust','ステータス:')!!}
-      {!! Form::select('status',[
-         'active' =>'active',
-         'idol'=>'idol',
-         'sleep'=>'sleep',
-         ],null,['class'=>'form-control'])
-  !!}
-  </div>
-
-
-    {!! Form::submit('投稿',['class'=>'btn btn-primary']) !!}
-
-
-    {!! Form::close() !!}
-	 </div><!--/col-xs-6-->
-  </div>
+    <div class="row">
+    	<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+      {!! Form::model($task,['route'=>'tasks.store']) !!}
+    
+    	<div class="form-group">
+        {!! Form::label('title','タイトル')!!}
+        {!! Form::text('title',null,['class'=>'form-control']) !!}
+         </div>
+         <div class="form-group">
+        {!! Form::label('content','タスク:')!!}
+        {!! Form::text('content',null,['class'=>'form-control']) !!}
+        </div>
+    	 <div class="form-group">
+          {!! Form::label('statust','ステータス:')!!}
+          {!! Form::select('status',[
+             'active' =>'active',
+             'idol'=>'idol',
+             'sleep'=>'sleep',
+             ],null,['class'=>'form-control'])
+      !!}
+      </div>
+    
+    
+        {!! Form::submit('投稿',['class'=>'btn btn-primary']) !!}
+    
+    
+        {!! Form::close() !!}
+    	 </div><!--/col-xs-6-->
+      </div>
 
 @endsection
